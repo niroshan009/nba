aea20dc92ca4e643dd9058ca79313732a3b9bffa
thusitha90 <dudeniro@gmail.com>
2020-04-30 20:36:22 +0800

    player profile page details update

--- src/app/player/player-detail.component.ts
+++ src/app/player/player-detail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IPlayer } from './Player';
+import { PlayerService } from './player.service';
 
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
-  styleUrls: ['./player-detail.component.css']
+  styleUrls: ['./player-detail.component.css'],
 })
 export class PlayerDetailComponent implements OnInit {
+  pageTitile: string = 'Player Overview';
+  player: IPlayer;
 
-  constructor() { }
+  constructor(
+    private router: ActivatedRoute,
+    private playerService: PlayerService
+  ) {}
 
   ngOnInit(): void {
+    let playerId = +this.router.snapshot.paramMap.get('id');
+    this.player = this.playerService.getPlayerById(playerId);
   }
-
 }
